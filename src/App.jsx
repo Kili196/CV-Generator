fed3777e5f526b416fea36a9222889bd022ae961
kili196 <kilian.baier16@gmail.com>
2025-02-15 11:58:24 +0100

    created user object to store details

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import "./styles/App.css";
 import "./styles/Form.css";
 import FormView from "./components/FormView";
+import { useState } from "react";
 
 function App() {
+  const [userDetails, setUserDetails] = useState({
+    firstname: "",
+    lastanem: "",
+    email: "",
+    phonenumber: "",
+  });
+
   return (
     <div className="container">
       <FormView />
