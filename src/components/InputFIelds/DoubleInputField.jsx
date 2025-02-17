d38786ef0fc8c14764e0583ecaba139e4563fb30
kili196 <kilian.baier16@gmail.com>
2025-02-17 19:01:23 +0100

    added required to DOubleInputFields

--- src/components/InputFIelds/DoubleInputField.jsx
+++ src/components/InputFIelds/DoubleInputField.jsx
@@ -3,15 +3,15 @@ const DoubleInputField = ({ subheading }) => {
     <div className="input-pair">
       <div>
         <label> {subheading}: </label>
-        <input type="text" />
+        <input type="text" required />
       </div>
       <div>
         <label> From: </label>
-        <input type="date" />
+        <input type="date" required />
       </div>
       <div>
         <label> To: </label>
-        <input type="date" />
+        <input type="date" required />
       </div>
     </div>
   );
