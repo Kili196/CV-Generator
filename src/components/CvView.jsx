9de5358ed1483698f880db57825a1c8fcc446e31
kili196 <kilian.baier16@gmail.com>
2025-03-01 23:16:24 +0100

    added information into header in cv view

--- src/components/CvView.jsx
+++ src/components/CvView.jsx
@@ -2,8 +2,29 @@ const CvView = () => {
   return (
     <div className="cv-view flex-center">
       <div className="cv-container">
-        <div className="cv-heading-section"></div>
-        <div className="cv-information-section"></div>
+        <header>
+          <div className="cv-details">
+            <div className="cv-detail">
+              <label className="cv-label">Name:</label>
+              <span className="cv-value">Kilian Baier</span>
+            </div>
+            <div className="cv-detail">
+              <label className="cv-label">Address:</label>
+              <span className="cv-value">Tulln, Sonnenstraße</span>
+            </div>
+            <div className="cv-detail">
+              <label className="cv-label">Email:</label>
+              <span className="cv-value">Kilian.baier16@gmail.com</span>
+            </div>
+            <div className="cv-detail">
+              <label className="cv-label">Phone Number:</label>
+              <span className="cv-value">+43 12 112 212 2</span>
+            </div>
+          </div>
+          <div className="cv-image"></div>
+        </header>
+
+        <main> </main>
       </div>
     </div>
   );
