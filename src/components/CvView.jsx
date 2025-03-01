b98d6fa012e788a006187bb489e2c06eac4a6281
kili196 <kilian.baier16@gmail.com>
2025-03-01 14:22:39 +0100

    added grid to my cv-view

--- src/components/CvView.jsx
+++ src/components/CvView.jsx
@@ -1,7 +1,10 @@
 const CvView = () => {
   return (
     <div className="cv-view flex-center">
-      <div className="cv-container"></div>
+      <div className="cv-container">
+        <div className="cv-heading-section"></div>
+        <div className="cv-information-section"></div>
+      </div>
     </div>
   );
 };
